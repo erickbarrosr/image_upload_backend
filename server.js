8a79313c8339772406ab557c7f33ffcd8729464c
Erick Barros <erickbarrosrezende@gmail.com>
2023-10-01 19:02:28 -0300

    fix [server]: removed server file

--- server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./app");
-const port = process.env.PORT || 3000;
-
-require("dotenv").config();
-
-app.listen(port, () => {
-  console.log(`O servidor está rodando na porta ${port}`);
-});
