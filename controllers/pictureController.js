a40010635037fac6b564d6d8adda1a7fa4d8e9e3
Erick Barros <erickbarrosrezende@gmail.com>
2023-10-01 21:35:01 -0300

    feat: add read and delete for images

--- controllers/pictureController.js
+++ controllers/pictureController.js
@@ -1,4 +1,5 @@
 const Picture = require("../models/Picture");
+const fs = require("fs");
 
 exports.create = async (req, res) => {
   try {
@@ -16,5 +17,34 @@ exports.create = async (req, res) => {
     res.json({ picture, message: "Imagem enviada com sucesso!" });
   } catch (error) {
     res.status(500).json({ message: "Erro ao enviar imagem!" });
+  }
+};
+
+exports.findAll = async (req, res) => {
+  try {
+    const pictures = await Picture.find();
+
+    res.json(pictures);
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao encontrar imagem!" });
+  }
+};
+
+exports.remove = async (req, res) => {
+  try {
+    const picture = await Picture.findById(req.params.id);
+
+    if (!picture) {
+      return res.status(404).json({ message: "Imagem não encontrada!" });
+    }
+
+    fs.unlinkSync(picture.source);
+
+    await picture.remove();
+
+    res.json({ message: "Imagem removida com sucesso!" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Erro ao excluir iamgem!" });
   }
 };
--- routes/picture.js
+++ routes/picture.js
@@ -5,5 +5,7 @@ const upload = require("../config/multer");
 const router = express.Router();
 
 router.post("/", upload.single("file"), pictureController.create);
+router.get("/", pictureController.findAll);
+router.delete("/:id", pictureController.remove);
 
 module.exports = router;
